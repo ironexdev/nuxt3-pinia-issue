6ed7aaf8011c13839f67ce40ea74efdfa0d1fb3e
Ironex <write.to.ironex@gmail.com>
2023-02-15 14:02:46 +0100

    Added: code to the query

--- middleware/authorization.global.ts
+++ middleware/authorization.global.ts
@@ -1,14 +1,14 @@
-import { defineNuxtRouteMiddleware } from "#app"
-import { storeToRefs } from "pinia"
-import { useAuthorizationStore } from "~/stores/AuthorizationStore"
+import {defineNuxtRouteMiddleware} from "#app"
+import {storeToRefs} from "pinia"
+import {useAuthorizationStore} from "~/stores/AuthorizationStore"
 
 export default defineNuxtRouteMiddleware(async (to) => {
-  if (to.path === "/") {
-      const authorizationStore = useAuthorizationStore()
-      const { code } = storeToRefs(authorizationStore)
+    if (to.path === "/") {
+        const authorizationStore = useAuthorizationStore()
+        const { code } = storeToRefs(authorizationStore)
 
-      code.value = "bar"
+        code.value = "bar"
 
-      return navigateTo("x")
-  }
+        return navigateTo({path: "x", query: {code: code.value}})
+    }
 })
